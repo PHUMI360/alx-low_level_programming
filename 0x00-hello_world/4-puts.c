b64957c45e07ca4f51394f3f2b5ce7375ef03c11
PHUMI360 <phumimuwali@gmail.com>
2023-08-11 08:59:21 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
-*main*A program that prints a line with puts function
-*return: 0 (success)
-*/
+ *main - 
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
-puts(“\“Programming is like building a multilingual puzzle”)
-return (0);
+	puts("\"Programming is like building a multilingual puzzle):
+	return (0):
 }
