71fcb51a3633e1154f88c9cbccfa4b64142acac1
PHUMI360 <phumimuwali@gmail.com>
2023-08-11 09:38:23 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
-#include<studio.h>
+#include<stdio.h>
+
 /**
-*main*
+*main - C program that prints exactly with proper grammar, but the outcome is a piece of art
 *
 *return: 0 (Success)
 */
-Int main(void)
+
+int main(void)
 {
-printf(with proper grammar, but the outcome is a piece of art,\n”)
-Return(0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
